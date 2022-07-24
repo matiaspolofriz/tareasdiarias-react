33dc0393f7f2698a0146c0d60dcd84c18326f919
Matias Polo Friz <polofrizmatias@gmail.com>
2022-07-24 15:46:51 +0200

    favicon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-import logo from "./imagenes/react.png";
+import logoMP from "./imagenes/icon-mp.png";
 import ListaDeTareas from "./componentes/ListaDeTareas";
 
 function App() {
   return (
     <div className="aplicacion-tareas">
       <div className="logo-contenedor">
-        <img className="logo" src={logo} alt="Logo React" />
+        <img className="logo" src={logoMP} alt="Logo React" />
       </div>
 
       <div className="tareas-lista-principal">
